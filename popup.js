5b4024c343fcd9f0201744b0c2a642fe9020c7b2
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-14 23:49:43 -0400

    adds error catching for urls for invalid pages of valid sites

--- popup.js
+++ popup.js
@@ -1,9 +1,11 @@
 // Populate textbox
 const populate = recipeData => {
   console.log(`POPULATE HIT`, recipeData)
-  if(recipeData){
+  if(recipeData && recipeData.recipe.includes(`•`) && recipeData.recipe.includes(`1)`)){
     document.getElementsByTagName(`textarea`)[0].innerHTML = recipeData.recipe
     document.getElementsByTagName(`input`)[0].value = `${recipeData.source.slice(0, -4)} ${recipeData.title}`
+  }else if(recipeData){
+    document.getElementsByTagName(`textarea`)[0].innerHTML = `Error: failed to scrape: invalid url\n\nMake sure you're using the url of a specific recipe`
   }
 }
 
@@ -19,24 +21,24 @@ window.addEventListener(`DOMContentLoaded`, () => {
 
 const download = () => {
   const text = document.getElementsByTagName(`textarea`)[0].innerHTML
-  const textAsBlob = new Blob([text], {type : `application/msword`})
+  const textAsBlob = new Blob([text], {type : `text/plain`})
   const fileNameToSaveAs = document.getElementsByTagName(`input`)[0].value
 
   const downloadLink = document.createElement(`a`)
   downloadLink.download = fileNameToSaveAs
-  downloadLink.innerHTML = `Download File`
-  if(window.webkitURL != null){
-    // Chrome allows the link to be clicked
-    // without actually adding it to the DOM.
-    downloadLink.href = window.webkitURL.createObjectURL(textAsBlob)
-  }else{
-    // Firefox requires the link to be added to the DOM
-    // before it can be clicked.
-    downloadLink.href = window.URL.createObjectURL(textAsBlob)
-    downloadLink.onclick = destroyClickedElement
-    downloadLink.style.display = `none`
-    document.body.appendChild(downloadLink)
-  }
+  downloadLink.innerHTML = `Download Recipe`
+  // if(window.webkitURL != null){
+  //   // Chrome allows the link to be clicked
+  //   // without actually adding it to the DOM.
+  //   downloadLink.href = window.webkitURL.createObjectURL(textAsBlob)
+  // }else{
+  //   // Firefox requires the link to be added to the DOM
+  //   // before it can be clicked.
+  downloadLink.href = window.URL.createObjectURL(textAsBlob)
+  downloadLink.onclick = destroyClickedElement
+  downloadLink.style.display = `none`
+  document.body.appendChild(downloadLink)
+  // }
 
   downloadLink.click()
 }
