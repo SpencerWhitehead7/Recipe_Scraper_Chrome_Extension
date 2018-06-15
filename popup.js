1539724259696b9e722a71ff82b1db09d52d2795
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-14 23:59:58 -0400

    refactors download

--- popup.js
+++ popup.js
@@ -20,30 +20,18 @@ window.addEventListener(`DOMContentLoaded`, () => {
 // Download text as .txt file
 
 const download = () => {
+  // Getting text into a downloadable format
   const text = document.getElementsByTagName(`textarea`)[0].innerHTML
   const textAsBlob = new Blob([text], {type : `text/plain`})
   const fileNameToSaveAs = document.getElementsByTagName(`input`)[0].value
 
+  // Triggering download
   const downloadLink = document.createElement(`a`)
   downloadLink.download = fileNameToSaveAs
   downloadLink.innerHTML = `Download Recipe`
-  // if(window.webkitURL != null){
-  //   // Chrome allows the link to be clicked
-  //   // without actually adding it to the DOM.
-  //   downloadLink.href = window.webkitURL.createObjectURL(textAsBlob)
-  // }else{
-  //   // Firefox requires the link to be added to the DOM
-  //   // before it can be clicked.
   downloadLink.href = window.URL.createObjectURL(textAsBlob)
-  downloadLink.onclick = destroyClickedElement
+  downloadLink.onclick = event => {document.body.removeChild(event.target)}
   downloadLink.style.display = `none`
   document.body.appendChild(downloadLink)
-  // }
-
   downloadLink.click()
-}
-
-function destroyClickedElement(event)
-{
-  document.body.removeChild(event.target)
 }
