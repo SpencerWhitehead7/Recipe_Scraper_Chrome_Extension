8b4b6d93301f7d47c98e4741b18cef566a00314f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-15 00:55:29 -0400

    enables extension on all supported sites

--- background.js
+++ background.js
@@ -1,12 +1,31 @@
 // Show/grey out button depending on whether site is supported
 
+/* eslint-disable complexity */ // Ignores "massively" complex tabs clause
 const show = (tabId, changeInfo, tab) => {
   chrome.tabs.query({active : true, currentWindow : true}, tabs => {
-    if(tabs[0].url.includes(`allrecipes.com`)){ // ADD THEM FOR ALL THE OTHER SUPPORTED SITES
+    if(tabs[0].url.includes(
+      `allrecipes.com` ||
+      `bettycrocker.com` ||
+      `boneappetit.com` ||
+      `chowhound.com` ||
+      `cookinglight.com` ||
+      `eatingwell.com` ||
+      `epicurious.com` ||
+      `food52.com` ||
+      `foodandwine.com` ||
+      `foodnetwork.com` ||
+      `geniuskitchen.com` ||
+      `jamieoliver.com` ||
+      `myrecipes.com` ||
+      `seriouseats.com` ||
+      `simplyrecipes.com` ||
+      `thekitchn.com`
+    )){
       chrome.pageAction.show(tabId)
     }
   })
 }
+/* eslint-enable complexity */
 
 chrome.tabs.onUpdated.addListener(show)
 
