6fd5556aef2f02971f8941cd6e638e7b9685a308
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-09-06 15:44:12 -0400

    edit: updates foodnetwork parser to work with their new html

--- content.js
+++ content.js
@@ -201,10 +201,8 @@ const foodandwine = recipe => {
 }
 
 const foodnetwork = recipe => {
-  recipe.title = $(`.o-AssetTitle__a-HeadlineText`).text().trim()
-  if(recipe.title.slice(-16) === `My Private Notes`){
-    recipe.title = recipe.title.slice(0, -16)
-  }
+  recipe.title = $(`.o-AssetTitle__a-HeadlineText`)[0].innerText // this works differently on the site, don't know why
+
   $(`.o-Ingredients__m-Body ul li`).each(function(){
     recipe.ingredients.push(`• ${$(this).text().trim()}`)
   })
@@ -213,14 +211,20 @@ const foodnetwork = recipe => {
       recipe.ingredients.push(`• ${$(this).text().trim()}`)
     })
   }
-  $(`.o-Method__m-Body p`).each(function(){
+
+  $(`.o-Method__m-Body ol li`).each(function(){
     recipe.instructions.push(`${$(this).text().trim()}`)
   })
-  if(recipe.instructions[recipe.instructions.length - 1].includes(`Photograph`)){ // to deal with the many FN.com recipes where the final "instruction" is a PC
-    recipe.instructions.pop() // this is an ugly and graceless fix, but they have ugly and graceless html and I'm doing the best I can
+  if(recipe.instructions.length === 0){ // /sigh other html layout
+    $(`.o-Method__m-Body p`).each(function(){
+      recipe.instructions.push(`${$(this).text().trim()}`)
+    })
   }
   if(recipe.instructions[0] === `Watch how to make this recipe.`){
     recipe.instructions.shift()
+  }
+  if(recipe.instructions[recipe.instructions.length - 1].includes(`Photograph`)){ // to deal with the many FN.com recipes where the final "instruction" is a PC
+    recipe.instructions.pop() // this is an ugly and graceless fix, but they have ugly and graceless html and I'm doing the best I can
   }
 }
 
